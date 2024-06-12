bb20618a2e9d3a930d81fea05913d1e5b482ab4e
Neeraj Sharma <neerajmudgal684@gmail.com>
2024-06-12 22:55:14 +0530

    Few Pages added

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -12,17 +12,17 @@ const Header = () =>{
                 </h1>
                 </Link>
                 <form className="bg-slate-100 p-3 rounded-lg flex items-center">
-                    <input type="search" placeholder="Search..." className="bg-transparent foucs:outline-none w-24 sm:w-64"/>
+                    <input type="search" placeholder="Search..." className="bg-transparent outline-none w-24 sm:w-64"/>
                     <FaSearch className="text-gray-500"/>
                 </form>
                 <ul className="flex gap-4">
                     <Link to={"/"}>
                         <li className="hidden sm:inline text-slate-700 hover:underline">Home</li>
                     </Link>
-                    <Link to={"/About"}>
+                    <Link to={"/about"}>
                         <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
                     </Link>
-                    <Link to={"/SignIn"}>
+                    <Link to={"/signin"}>
                         <li className="text-slate-700 hover:underline">Sign In</li>
                     </Link>
                 </ul>
